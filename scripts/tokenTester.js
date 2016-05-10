071e4514e1281a517c428fa5031b5efe52b10418
Bruce McPherson <bruce@mcpher.com>
2016-05-10 16:59:48 +0100

    updated by GasGit automation

--- scripts/tokenTester.js
+++ scripts/tokenTester.js
@@ -2,13 +2,17 @@ function tokenTester() {
   var goa = cGoa.make(Demo.PACKAGE_APP.name,Demo.PACKAGE_APP.props);
   //Logger.log(goa.getPackage());
   Logger.log(goa.hasToken());
+
   
   var goa = cGoa.make(Demo.PACKAGE_RUN.name,Demo.PACKAGE_RUN.props);
   //Logger.log(goa.getPackage());
   Logger.log(goa.hasToken());
+
   
   var goa = cGoa.make(Demo.PACKAGE_PLAY.name,Demo.PACKAGE_PLAY.props);
   //Logger.log(goa.getPackage());
   Logger.log(goa.hasToken());
+
+
   
 }
